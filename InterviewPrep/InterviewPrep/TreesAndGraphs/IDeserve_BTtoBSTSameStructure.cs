458339fcc2a60973bf40ad88a3b599b3dad3f5da
smahesh11 <smahesh5@asu.edu>
2017-05-02 19:12:25 -0700

    Add files via upload

---
+++ InterviewPrep/InterviewPrep/TreesAndGraphs/IDeserve_BTtoBSTSameStructure.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrep.TreesAndGraphs
+{
+    public class BTtoBSTSameStructure
+    {
+        List<int> inorder = new List<int>();
+        public BinaryTreNode ToBST(BinaryTreNode root)
+        {
+            int[] index = new int[1];
+          
+            ConvertToInorder(root, inorder, index);
+
+            inorder.Sort();
+            index[0] = 0;
+
+            ReplaceNodes(root, inorder, index);
+
+            return root;
+        }
+
+        public void ConvertToInorder(BinaryTreNode curr, List<int> inorder, int[] index)
+        {
+            if (curr == null)
+                return;
+
+            ConvertToInorder(curr.GetLeft, inorder, index);
+            inorder.Add(curr.GetData);
+            index[0]++;
+            ConvertToInorder(curr.GetRight, inorder, index);
+        }
+
+        public void ReplaceNodes(BinaryTreNode curr, List<int> inorder, int[] i)
+        {
+            if (curr == null)
+                return;
+
+            ReplaceNodes(curr.GetLeft, inorder, i);
+
+            curr.GetData = inorder[i[0]];
+            i[0] += 1;
+
+            ReplaceNodes(curr.GetRight, inorder, i);
+        }
+    }
+
+
+}
---
+++ InterviewPrep/InterviewPrep/TreesAndGraphs/IDeserve_OnlyOneChild.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrep.TreesAndGraphs
+{
+    public class IfHasOnlyOneChildPerNode
+    {
+        public bool CheckIfOnlyOneChild(int[] array)
+        {
+            int max = array[array.Length - 1];
+            int min = array[array.Length - 1];
+
+            for (int i = array.Length - 2; i >= 0; i--)
+            {
+                if ((array[i] < max) && array[i] > min)
+                    return false;
+
+                else
+                {
+                    max = Math.Max(array[i], max);
+                    min = Math.Min(array[i], min);
+                }
+            }
+            return true;           
+        }
+    }
+}
